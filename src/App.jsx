a14f6456cc42e243d38dca845871936d6b4b1740
Timur Shomakhov <timur-colmberg@web.de>
2025-03-03 12:22:21 +0100

    Adding the post details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import CreatePost from "./pages/CreatePost";
+import PostDetails from "./pages/PostDetails";
 
 function App() {
   const [posts, setPosts] = useState([]);
 
   const addPost = (post) => {
     setPosts([...posts, post]);
+  };
+
+  const updatePost = (id, newTitle, newContent) => {
+    setPosts(posts.map(post => post.id === id ? { ...post, title: newTitle, content: newContent } : post));
+  };
+
+  const deletePost = (id) => {
+    setPosts(posts.filter(post => post.id !== id));
   };
 
   return (
@@ -19,6 +28,7 @@ function App() {
       <Routes>
         <Route path="/" element={<h1 className="p-4">Welcome to the Blog</h1>} />
         <Route path="/create" element={<CreatePost addPost={addPost} />} />
+        <Route path="/post/:id" element={<PostDetails posts={posts} updatePost={updatePost} deletePost={deletePost} />} />
       </Routes>
     </Router>
   );
